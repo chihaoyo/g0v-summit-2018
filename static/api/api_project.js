886da51d25b380c9652920ca331733012762c2ab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-09 05:47:34 +0000

    Deploy g0v/summit2018 to github.com/g0v/summit2018.git:gh-pages

---
+++ static/api/api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "API - g0v summit 2018",
+  "version": "0.1.0",
+  "description": "提供 2018 summit 官網上就有的各種資料。主要是為了避免有人覺得「雖然是網站前端就有的資料，如果可以用 json 的格式拿到叫做安全漏洞」，所以加了這份文件把它變成 feature，不是很有用的一套 API。but still, like open source project always says, 'We are excited about what you will(can) build with it!'",
+  "apidoc": "0.3.0",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-09-09T05:46:31.248Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
